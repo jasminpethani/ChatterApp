f6cad6c97c4d3762ab60c992645c8baadd19481f
jazzp561 <jazz.pethani@gmail.com>
2018-03-10 21:40:49 +0530

    Initial Commit

---
+++ ChatterApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChatterApp
+//
+//  Created by jasmin on 10/03/18.
+//  Copyright © 2018 jasmin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+     func applicationDidFinishLaunching(_ aNotification: Notification) {
+          // Insert code here to initialize your application
+     }
+
+     func applicationWillTerminate(_ aNotification: Notification) {
+          // Insert code here to tear down your application
+     }
+
+
+}
+
---
+++ ChatterApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChatterApp
+//
+//  Created by jasmin on 10/03/18.
+//  Copyright © 2018 jasmin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+
+          // Do any additional setup after loading the view.
+     }
+
+     override var representedObject: Any? {
+          didSet {
+          // Update the view, if already loaded.
+          }
+     }
+
+
+}
+
